b2ade5819db7f71ef770a3741f81921795d67922
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-07-21 12:24:03 +0530

    Create Stack.py

---
+++ Data Structures/Stack.py
@@ -0,0 +1,21 @@
+class Stack:
+
+	def __init__(self):
+		self.stack = []
+		
+	def isEmpty(self):
+		return self.stack == []
+		
+	def push(self, data):
+		self.stack.append(data)
+		
+	def pop(self):
+		data = self.stack[-1]
+		del self.stack[-1]
+		return data
+		
+	def peek(self):
+		return self.stack[-1]
+		
+	def sizeStack(self):
+		return len(self.stack)
