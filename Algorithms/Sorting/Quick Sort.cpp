6d8123631a8209f3869c8a292cf98863efaf3354
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-07-21 13:19:45 +0530

    Update Quick Sort.cpp

--- Algorithms/Sorting/Quick Sort.cpp
+++ Algorithms/Sorting/Quick Sort.cpp
@@ -3,7 +3,7 @@ void quickSort(int arr[], int low, int high) {
     if (low < high)     {
         // pi is partitioning index, arr[p] is now  at right place
         int pi = partition(arr, low, high);
-        // Separately sort elements before / partition and after partition
+        // Separately sort elements before  partition and after partition
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
