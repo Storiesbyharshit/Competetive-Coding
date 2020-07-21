4922dc822c17517c9cde9e03631e65282a8b3231
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-07-21 12:43:32 +0530

    Update Bubble Sort.py

--- Algorithms/Sorting/Bubble Sort.py
+++ Algorithms/Sorting/Bubble Sort.py
@@ -3,12 +3,12 @@ def bubble_sort(nums):
     
 	for i in range(len(nums)-1):
 		for j in range(len(nums)-1-i):
-			if nums[j] > nums[j+1]:
+			if nums[j] > nums[j+1]: #swap if element on the left is greater than element on the right
 				swap(nums, j, j+1)
 	
 	return nums
 	
-def swap(nums, i, j):
+def swap(nums, i, j): #helper function
 	temp = nums[i]
 	nums[i] = nums[j]
 	nums[j] = temp
