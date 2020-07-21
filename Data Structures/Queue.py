8499cdf1a74f952465e23d32914d5087d5a35daa
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-07-21 12:24:54 +0530

    Create Queue.py

---
+++ Data Structures/Queue.py
@@ -0,0 +1,23 @@
+#FIFO - first -> first
+
+class Queue:
+
+	def __init__(self):
+		self.queue = []
+		
+	def isEmpty(self):
+		return self.queue == []
+		
+	def enqueue(self, data):
+		self.queue.append(data)
+		
+	def dequeue(self):
+		data = self.queue[0]
+		del self.queue[0]
+		return data
+		
+	def peek(self):
+		return self.queue[0]
+		
+	def sizeQueue(self):
+		return len(self.queue)
