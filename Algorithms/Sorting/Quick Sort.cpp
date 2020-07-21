bacffb76fca8f7ca557a0afa85a506734968e24d
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-07-21 13:19:24 +0530

    Create Quick Sort.cpp

---
+++ Algorithms/Sorting/Quick Sort.cpp
@@ -0,0 +1,34 @@
+// arr[] --> Array to be sorted,    low  --> Starting index,   high  --> Ending index
+void quickSort(int arr[], int low, int high) {
+    if (low < high)     {
+        // pi is partitioning index, arr[p] is now  at right place
+        int pi = partition(arr, low, high);
+        // Separately sort elements before / partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];    // pivot
+    int i = (low - 1);  // Index of smaller element
+    for (int j = low; j <= high- 1; j++)
+    {
+        // If current element is smaller than or equal to pivot
+        if (arr[j] <= pivot)
+        {
+            i++;    // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
