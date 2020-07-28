072f636caefb0c99b8d4c3fbe8518b52a032112c
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-07-28 22:37:28 +0530

    Update 35. Search Insert Position.py

--- LeetCode-Solutions/35. Search Insert Position.py
+++ LeetCode-Solutions/35. Search Insert Position.py
@@ -1,5 +1,17 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        index = 0
+        
+        for num in nums:
+            if num >= target:
+                return index
+            index += 1
+        
+        if index == len(nums):
+            return len(nums)
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
         if target in nums:
             return nums.index(target)
         else:
