60dfb91ce2b8b43aa2377aa2724b58486c8ce1ce
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-08-08 22:57:33 +0530

    Update 257. Binary Tree Paths.py

--- LeetCode-Solutions/257. Binary Tree Paths.py
+++ LeetCode-Solutions/257. Binary Tree Paths.py
@@ -4,6 +4,37 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
+        def construct_paths(root,path):
+            if root:
+                path+=str(root.val)
+                if not root.left and not root.right:
+                    paths.append(path)
+                else:
+                    path+='->'
+                    if root.left:
+                        construct_paths(root.left,path)
+                    if root.right:
+                        construct_paths(root.right,path)
+        paths=[]
+        construct_paths(root,'')
+        return paths
+    
+    
+    
+    
+    
+    
+    
+    
+    
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 class Solution:
     def binaryTreePaths(self, root):
