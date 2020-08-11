887dfdbc97b37c5fa141065343f997adb340b851
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-08-11 22:47:55 +0530

    Update frequencycount.py

--- GeeksforGeeks/Array/frequencycount.py
+++ GeeksforGeeks/Array/frequencycount.py
@@ -3,4 +3,11 @@ def frequencycount(A,N):
     import collections
     freq_dict = collections.Counter(A)
     return freq_dict.values()
-    
+
+
+def printfrequency(A,N):
+    newArr = [0] * N
+    for i in range(N):
+        newArr[A[i]-1] +=1
+    for ele in newArr:
+        print(ele, end=" ")
