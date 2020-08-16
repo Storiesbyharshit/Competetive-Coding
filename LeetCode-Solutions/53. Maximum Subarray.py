e8483fa31545fb0ab91e820a7ce8b2bc568d1c87
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-08-16 17:07:20 +0530

    Update 53. Maximum Subarray.py

--- LeetCode-Solutions/53. Maximum Subarray.py
+++ LeetCode-Solutions/53. Maximum Subarray.py
@@ -12,4 +12,14 @@ class Solution:
                 localmax = 0
             
         return globalmax
+    
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res = nums[0]
+        maxend = nums[0]
+        
+        for i in range(1,len(nums)):
+            maxend = max(maxend+nums[i],nums[i])
+            res = max(res,maxend)
+        return res
             
