68791c6bb908587df9a67b9c8b5b6a13665cf142
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-08-20 08:58:00 +0530

    Create 0 - 1 Knapsack.py

---
+++ GeeksforGeeks/Dynamic Programming/0 - 1 Knapsack.py
@@ -0,0 +1,38 @@
+"""
+0 - 1 Knapsack Problem 
+You are given weights and values of N items, put these items in a knapsack 
+of capacity W to get the maximum total value in the knapsack. Note that we have only one quantity of each item.
+In other words, given two integer arrays val[0..N-1] and wt[0..N-1] which represent values and weights 
+associated with N items respectively. Also given an integer W which represents knapsack capacity, 
+find out the maximum value subset of val[] such that sum of the weights of this subset is smaller
+than or equal to W. You cannot break an item, either pick the complete item, or don’t pick it (0-1 property).
+
+The complete solution:
+
+There are 2 cases for every item : either it is included in the bag or excluded.
+Therefore, the maximum value that can be obtained from n items is max of following two values.
+1) Maximum value obtained by n-1 items and W weight (excluding nth item).
+2) Value of nth item plus maximum value obtained by n-1 items and W minus weight of the nth item (including nth item).
+Iterate the DP array for all of W and N, store the value at DP[w][n] as max of above 2 conditions.
+Return DP[W][N].
+This is the solution.
+
+"""
+#Python 3
+# A Dynamic Programming based Python Program for 0-1 Knapsack problem
+# Returns the maximum value that can be put in a knapsack of capacity W
+def knapSack(W, wt, val, n):
+    K = [[0 for x in range(W + 1)] for x in range(n + 1)]
+
+    # Build table K[][] in bottom up manner
+    for i in range(n + 1):
+        for w in range(W + 1):
+            if i == 0 or w == 0:
+                K[i][w] = 0
+            elif wt[i - 1] <= w:
+                K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w])
+            else:
+                K[i][w] = K[i - 1][w]
+
+    return K[n][W]
+
