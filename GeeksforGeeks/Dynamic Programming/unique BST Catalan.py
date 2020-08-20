ada55a019b74c8ca0d9fadfebc4031920e40256e
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-08-20 07:55:58 +0530

    Create unique BST Catalan.py

---
+++ GeeksforGeeks/Dynamic Programming/unique BST Catalan.py
@@ -0,0 +1,10 @@
+"""Total number of possible Binary Search Trees with n different keys (countBST(n)) = Catalan number Cn = (2n)!/(n+1)!*n!
+
+For n = 0, 1, 2, 3, … values of Catalan numbers are 1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862, …. So are numbers of Binary Search Trees.
+
+Total number of possible Binary Trees with n different keys (countBT(n)) = countBST(n) * n!
+
+ 
+Unique BST's 
+Given an integer. Find how many structurally unique binary search trees are there that stores the values from 1 to that integer (inclusive). u
+"""
