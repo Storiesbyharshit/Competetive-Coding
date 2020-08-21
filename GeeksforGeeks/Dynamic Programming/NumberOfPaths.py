609c7ca04b9df9b1f9a769daa13e6bfe175bf39f
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-08-21 07:34:57 +0530

    Update NumberOfPaths.py

--- GeeksforGeeks/Dynamic Programming/NumberOfPaths.py
+++ GeeksforGeeks/Dynamic Programming/NumberOfPaths.py
@@ -13,3 +13,26 @@ def NumberOfPaths(a, b):
         for j in range(1, b):
             ans[i][j] = ans[i-1][j]+ans[i][j-1]
     return ans[a-1][b-1]
+    
+def numberOfPaths(m, n): 
+    # Create a 2D table to store 
+    # results of subproblems 
+    count = [[0 for x in range(m)] for y in range(n)] 
+    
+    # Count of paths to reach any  
+    # cell in first column is 1 
+    for i in range(m): 
+        count[i][0] = 1; 
+    
+    # Count of paths to reach any  
+    # cell in first column is 1 
+    for j in range(n): 
+        count[0][j] = 1; 
+    
+    # Calculate count of paths for other 
+    # cells in bottom-up  
+    # manner using the recursive solution 
+    for i in range(1, m): 
+        for j in range(1, n):              
+            count[i][j] = count[i-1][j] + count[i][j-1] 
+    return count[m-1][n-1] 
