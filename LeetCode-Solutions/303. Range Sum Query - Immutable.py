b84d30660d37c22c41a7fddb31a78f063ec901ad
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-08-21 22:40:33 +0530

    Create 303. Range Sum Query - Immutable.py

---
+++ LeetCode-Solutions/303. Range Sum Query - Immutable.py
@@ -0,0 +1,19 @@
+#303. Range Sum Query - Immutable
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        
+        self.dp=[0]
+        
+        for num in nums:
+            self.dp.append(self.dp[-1]+num)
+
+        
+        
+    def sumRange(self, i: int, j: int) -> int:
+        
+        return self.dp[j+1]-self.dp[i]
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(i,j)
