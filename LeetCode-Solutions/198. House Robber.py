bba5f337ee966466727b73dcb90b6d07809eb80a
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-08-21 22:31:42 +0530

    Create 198. House Robber.py

---
+++ LeetCode-Solutions/198. House Robber.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums)
+        
+        dp = {}
+        dp[0] = nums[0]
+        dp[1] = max(nums[0],nums[1])
+        
+        for i in range(2,n):
+            dp[i] = max(nums[i]+dp[i-2],dp[i-1])
+        return dp[i]
