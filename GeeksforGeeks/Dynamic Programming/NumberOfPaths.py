7d0adcebc30f332389ec71cce2311bdea2449dc4
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-08-21 07:37:42 +0530

    Update NumberOfPaths.py

--- GeeksforGeeks/Dynamic Programming/NumberOfPaths.py
+++ GeeksforGeeks/Dynamic Programming/NumberOfPaths.py
@@ -36,3 +36,21 @@ def numberOfPaths(m, n):
         for j in range(1, n):              
             count[i][j] = count[i-1][j] + count[i][j-1] 
     return count[m-1][n-1] 
+
+
+
+
+
+
+
+
+def numberOfPaths(m, n): 
+# If either given row number is first 
+# or given column number is first 
+    if(m == 1 or n == 1): 
+        return 1
+  
+# If diagonal movements are allowed 
+# then the last addition 
+# is required. 
+    return numberOfPaths(m-1, n) + numberOfPaths(m, n-1) 
