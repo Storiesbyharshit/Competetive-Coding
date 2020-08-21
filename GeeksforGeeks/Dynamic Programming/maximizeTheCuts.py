d8f1b9548326d7d71ee73709fe5b4d7713f4051c
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-08-21 08:10:53 +0530

    Update maximizeTheCuts.py

--- GeeksforGeeks/Dynamic Programming/maximizeTheCuts.py
+++ GeeksforGeeks/Dynamic Programming/maximizeTheCuts.py
@@ -58,3 +58,45 @@ def maximizeTheCuts( l, p, q, r):
     if(dp[l]==-1):
         return 0
     return dp[l] 
+
+
+
+# Python implementation  
+# to divide N into maximum  
+# number of segments of  
+# length a, b and c 
+  
+# function to find  
+# the maximum number  
+# of segments 
+def maximumSegments(n, a, b, c) : 
+  
+    # stores the maximum  
+    # number of segments  
+    # each index can have 
+    dp = [-1] * (n + 10) 
+  
+    # 0th index will have  
+    # 0 segments base case 
+    dp[0] = 0
+  
+    # traverse for all possible 
+    # segments till n 
+    for i in range(0, n) : 
+      
+        if (dp[i] != -1) : 
+          
+            # conditions 
+            if(i + a <= n ): # avoid buffer overflow     
+                dp[i + a] = max(dp[i] + 1,  
+                            dp[i + a]) 
+                              
+            if(i + b <= n ): # avoid buffer overflow     
+                dp[i + b] = max(dp[i] + 1,  
+                            dp[i + b]) 
+                              
+            if(i + c <= n ): # avoid buffer overflow     
+                dp[i + c] = max(dp[i] + 1,  
+                            dp[i + c]) 
+  
+    return dp[n]
