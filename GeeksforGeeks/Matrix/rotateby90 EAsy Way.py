2ec2c87110834fc3023a464e64ac5fe5e476630d
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-09-05 08:06:00 +0530

    Update rotateby90 EAsy Way.py

--- GeeksforGeeks/Matrix/rotateby90 EAsy Way.py
+++ GeeksforGeeks/Matrix/rotateby90 EAsy Way.py
@@ -1,3 +1,11 @@
+class Solution:
+    def rotate(self, matrix):
+        matrix.reverse()
+        for i in range(len(matrix)):
+            for j in range(i+1,len(matrix)):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+ 
 def rotateby90(a, n): 
     #Your code here
     
@@ -10,3 +18,16 @@ def rotateby90(a, n):
         a[i], a[j] = a[j], a[i]
         i+=1
         j-=1
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        for i in range(0,len(matrix)//2):
+            temp = matrix[i]
+            matrix[i] = matrix[len(matrix)-1-i]
+            matrix[len(matrix)-1-i] = temp
+        
+        for i in range(len(matrix)):
+            for j in range(i+1,len(matrix)):
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[j][i]
+                matrix[j][i] = temp
