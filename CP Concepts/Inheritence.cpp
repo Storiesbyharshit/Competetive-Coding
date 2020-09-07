daafdc33e2929bbe8405de66eb12d3441f35e3c5
Harshit Singh <63466749+Storiesbyharshit@users.noreply.github.com>
2020-09-07 14:44:23 +0530

    Create Inheritence.cpp

---
+++ CP Concepts/Inheritence.cpp
@@ -0,0 +1,42 @@
+Live Demo
+#include <iostream>
+ 
+using namespace std;
+
+// Base class
+class Shape {
+   public:
+      void setWidth(int w) {
+         width = w;
+      }
+      void setHeight(int h) {
+         height = h;
+      }
+      
+   protected:
+      int width;
+      int height;
+};
+
+// Derived class
+class Rectangle: public Shape {
+   public:
+      int getArea() { 
+         return (width * height); 
+      }
+};
+
+int main(void) {
+   Rectangle Rect;
+ 
+   Rect.setWidth(5);
+   Rect.setHeight(7);
+
+   // Print the area of the object.
+   cout << "Total area: " << Rect.getArea() << endl;
+
+   return 0;
+}
+When the above code is compiled and executed, it produces the following result −
+
+Total area: 35
